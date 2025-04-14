456afa258113f91a415d139b0e31430e942861d1
samucj73 <samu.rcj@gmail.com>
2025-04-14 09:11:28 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Instalar dependências necessárias
+# Atualiza repositórios e instala dependências
 apt-get update
 apt-get install -y unzip curl
 
-# Instalar Chrome
+# Instalar o Google Chrome
 mkdir -p .render/chrome
 cd .render/chrome
 curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb || true
+apt-get install -y ./google-chrome-stable_current_amd64.deb || true
 cd ../..
 
-# Descobrir versão do Chrome
+# Descobrir a versão principal do Chrome
 CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+')
 CHROME_MAJOR=$(echo $CHROME_VERSION | cut -d '.' -f 1)
 
-# Instalar Chromedriver compatível
+# Instalar o Chromedriver compatível
 mkdir -p .render/chromedriver
 cd .render/chromedriver
 CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_MAJOR")
