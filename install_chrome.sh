a4ead826cc4d7c12fdaefdccff8079278f350537
samucj73 <samu.rcj@gmail.com>
2025-04-14 10:23:57 -0300

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-# Atualiza os repositórios e instala dependências
+# Atualiza o sistema e instala dependências
 apt-get update
-apt-get install -y unzip curl
+apt-get install -y wget curl unzip gnupg
 
-# Instalar Google Chrome
-mkdir -p .render/chrome
-cd .render/chrome
-curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# Instala o Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -y ./google-chrome-stable_current_amd64.deb || true
-cd ../..
 
-# Descobrir a versão principal do Chrome
+# Cria link simbólico para facilitar a localização do Chrome
+ln -s /usr/bin/google-chrome /usr/local/bin/chrome
+
+# Descobre a versão do Chrome instalada
 CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+')
 CHROME_MAJOR=$(echo $CHROME_VERSION | cut -d '.' -f 1)
 
-# Instalar Chromedriver compatível
-mkdir -p .render/chromedriver
-cd .render/chromedriver
+# Baixa e instala o Chromedriver compatível
 CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_MAJOR")
-curl -O "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
-unzip chromedriver_linux64.zip
+wget -O chromedriver.zip "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
+unzip chromedriver.zip
 chmod +x chromedriver
-cd ../..
+mv chromedriver /usr/local/bin/
