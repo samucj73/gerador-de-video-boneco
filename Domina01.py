7dda3906575fa8172ab7431d8fcb1de619ab0acb
samucj73 <samu.rcj@gmail.com>
2025-08-29 21:38:35 -0300

    Update Domina01.py

--- Domina01.py
+++ Domina01.py
@@ -287,7 +287,7 @@ if entrada_info:
             numeros_terminal = sorted([n for n in range(37) if n % 10 == t])
             linhas_numeros.append(f"T{t}: {' '.join(map(str, numeros_terminal))}")
 
-        msg_alerta = "🎯 Previsão Terminais Dominantes\n" + "\n".join(linhas_numeros)
+        msg_alerta = "Previsão\n" + "\n".join(linhas_numeros)
         enviar_previsao(msg_alerta)
 
     # Critério C: 13º número não bate com os 12 anteriores -> zerar entrada e aguardar próximo giro
